fcb6067b000f64020adf2ed942ae048d13804876
Aurelien Morvan <aurelien.morvan@infostrates.fr>
2019-09-20 16:29:11 +0200

    Test Other Screen

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,10 +1,12 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import HomeScreenView from "../view/screens/HomeScreenView";
+import OtherScreenView from "../view/screens/OtherScreenView";
 
 
 const Navigator = createBottomTabNavigator({
     Home: {screen: HomeScreenView},
+    Other: {screen: OtherScreenView}
 });
 
 export default createAppContainer(Navigator);
---
+++ src/view/screens/OtherScreenView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class OtherScreenView extends React.Component {
+    render() {
+        return <View>
+            <Text>Autre écran</Text>
+        </View>
+    }
+}
